a750ba43bf977e06aaf88755a82935aa0f66a9e8
Manish Sharma <Manish.X.Sharma@JohnLewis.co.uk>
2024-08-14 12:58:34 +0530

    Added sql instance, secret manager module

--- envs/non-prod-non-phi/cloud-sql/main.tf
+++ envs/non-prod-non-phi/cloud-sql/main.tf
@@ -1,9 +1,31 @@
-module "private_service_connect" {
-  source = "../../modules/cloud-sql"
-
+module "google_sql_database_instance" {
+    source = "../../modules/cloud-sql"
+  for_each =      var.instance_name
   project_id        = var.project #"your-gcp-project-id"
   vpc_name          = var.vpc_name #"your-vpc-name"
   subnet_name       = var.subnet_name #"your-subnet-name"
   region            = var.region #"us-central1"
+  db_instance_name = lookup(each.value, "instance_name", null)
   psc_target_service = var.psc_target_service #"your-target-service"
 }
+
+module "google_secret_manager_secret" {
+    source = "../../modules/cloud-sql"
+  for_each =      var.instance_name
+  project_id        = var.project #"your-gcp-project-id"
+  region            = var.region #"us-central1"
+  db_instance_name = lookup(each.value, "instance_name", null)
+  secret_data = var.db_master_password
+  
+}
+module "google_sql_user" {
+    source = "../../modules/cloud-sql"
+  for_each =      var.instance_name
+  project_id        = var.project #"your-gcp-project-id"
+  region            = var.region #"us-central1"
+  db_instance_name = lookup(each.value, "instance_name", null)
+  name     = var.db_master_username
+
+}
+
+
--- envs/non-prod-non-phi/cloud-sql/variables.tf
+++ envs/non-prod-non-phi/cloud-sql/variables.tf
@@ -18,3 +18,33 @@ variable "psc_target_service" {
     type = string
     description = "Name of the service to connect to"
 }
+
+variable "instance_name" {
+  type = map(string)
+  default = {
+    instance1= {
+      name = string
+    }
+    instance2 = {
+      name = string
+    }
+    instance3 = {
+      name = string
+    }
+  }
+}
+
+variable "namespaces" {
+  type = map(string)
+  default = {
+    namespace1= {
+      name = string
+    }
+    namespace2 = {
+      name = string
+    }
+    namespace3 = {
+      name = string
+    }
+  }
+}
